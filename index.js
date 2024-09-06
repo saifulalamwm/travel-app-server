cd33648f96c0ae5196ca98b32473264d1b18a516
saifulalamwm <159639800+saifulalamwm@users.noreply.github.com>
2024-09-06 23:32:19 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-require("dotenv").config();
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@travel-app.slng6tq.mongodb.net/?retryWrites=true&w=majority&appName=travel-app`;
 
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
 
 app.get("/", (req, res) => {
-  res.send(`User management server is running ${port}`);
+  res.send(`Travel App running on port ${port}`);
 });
 
 // Mongo db connection
 
-const uri =
-  "mongodb+srv://travelapp24:vqrCxWPzBmVR5urx@travel-app-24.h3cwe5f.mongodb.net/?retryWrites=true&w=majority&appName=Travel-App-24";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@travel-app-24.h3cwe5f.mongodb.net/?retryWrites=true&w=majority&appName=Travel-App-24`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,9 +28,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const travelCollection = client.db("spotsDB").collection("spots");
+    const myListCollection = client.db("spotsDB").collection("myList");
 
     // create a get connection
 
@@ -47,7 +46,34 @@ async function run() {
       const newSpot = req.body;
       const result = await travelCollection.insertOne(newSpot);
       res.send(result);
-      console.log(newSpot);
+    });
+
+    //post myList to myList collection DB
+    app.post("/myList", async (req, res) => {
+      const myList = req.body;
+      const query = { listId: myList.listId, email: myList.email };
+      const existingList = await myListCollection.findOne(query);
+      if (existingList) {
+        return res.send({ message: "already listed", insertedId: null });
+      }
+      const result = await myListCollection.insertOne(myList);
+      res.send(result);
+    });
+
+    //get from myList
+    app.get("/myList", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await myListCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //delete for list
+    app.delete("/myList/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await myListCollection.deleteOne(query);
+      res.send(result);
     });
 
     // get single data
@@ -55,6 +81,22 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await travelCollection.findOne(query);
+      res.send(result);
+    });
+
+    //delete spots
+    app.delete("/myUpdates/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await travelCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //get sports by email
+    app.get("/myUpdates", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await travelCollection.find(query).toArray();
       res.send(result);
     });
 
@@ -86,10 +128,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
